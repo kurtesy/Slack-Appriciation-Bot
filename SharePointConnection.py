3187b7fb1215e2f243ab63bca23e11d56869e425
Nishant Patel <np210291@gmail.com>
2020-05-04 11:50:36 +0530

    Update SharePointConnection.py

--- SharePointConnection.py
+++ SharePointConnection.py
@@ -1,6 +1,6 @@
 from sharepoint import SharePointSite, basic_auth_opener
 url="https://fcs.allegisgroup.net/practices/biepm/Lists/Profile/Active%20Profile.aspx"
-user, pwd = "corporate\\npatel", "qwerty@123"
+user, pwd = "user", "pwd"
 
 opener = basic_auth_opener(url, user, pwd)
 
@@ -8,16 +8,3 @@ site = SharePointSite(url, opener)
 for sp_list in site.lists:
     print sp_list.id, sp_list.meta['Title']
 
-
-
-
-"""import requests
-from requests_ntlm import HttpNtlmAuth
-
-url="https://fcs.allegisgroup.net/practices/biepm/Lists/Profile/Active%20Profile.aspx"
-user, pwd = "corporate\\npatel", "qwerty@123"
-headers = {'accept': 'application/json;odata=verbose'}
-r = requests.get(url, auth=HttpNtlmAuth(user, pwd), headers=headers)
-print r.status_code
-print r.content
-print r.json()"""
